5d106947a83929392947c27aaee68294b0bbb0a4
phi long <long9ka@gmail.com>
2021-01-18 23:42:11 +0700

    CRUD vehicle

---
+++ api/routes/vehicles/controllers/vehicles.js
@@ -0,0 +1,85 @@
+// Models
+const Vehicle = require('../../../models/Vehicle');
+
+async function getVehicles(req, res) {
+  try {
+    const vehicles = await Vehicle.find();
+    if (vehicles) {
+      res.json({ vehicles });
+    }
+  } catch (err) {
+    res.status(404).json({
+      msg: 'Vehicles Do Not Exist'
+    });
+  }
+}
+
+async function getVehicleById(req, res) {
+  try {
+    const vehicle = await Vehicle.findById(req.params.id);
+    if (vehicle) {
+      res.json({ vehicle });
+    }
+  } catch (err) {
+    res.status(404).json({
+      msg: 'Vehicle Does Not Exist'
+    });
+  }
+}
+
+async function createVehicle(req, res) {
+  try {
+    // Create vehicle
+    const newVehicle = new Vehicle({
+      type: req.body.type
+    });
+    await newVehicle.save();
+
+    res.status(201).json({
+      vehicle: newVehicle
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: 'Server error'
+    });
+  }
+}
+
+async function updateVehicleById(req, res) {
+  try {
+    await Vehicle.findByIdAndUpdate(req.params.id, {
+      type: req.body.type
+    });
+
+    res.json({
+      msg: 'Update vehicle success'
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: 'Server error'
+    });
+  }
+}
+
+async function deleteVehicleById(req, res) {
+  try {
+    await Vehicle.findByIdAndRemove(req.params.id);
+
+    res.json({
+      msg: 'Delete vehicle success'
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: 'Server error'
+    });
+  }
+}
+
+
+module.exports = {
+  getVehicles,
+  getVehicleById,
+  createVehicle,
+  updateVehicleById,
+  deleteVehicleById
+}
---
+++ api/routes/vehicles/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+// Middleware
+const auth = require('../users/middlewares/auth');
+// Controllers
+const vhcCtrl = require('./controllers/vehicles');
+
+router.route('/')
+  .get(auth, vhcCtrl.getVehicles)
+  .post(auth, vhcCtrl.createVehicle)
+  .put()
+  .delete();
+
+router.route('/:id')
+  .get()
+  .post()
+  .put(auth, vhcCtrl.updateVehicleById)
+  .delete(auth, vhcCtrl.deleteVehicleById);
+
+module.exports = router;
